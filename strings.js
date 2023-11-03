efb4be9a70db574830be401642f2f14d1ff0e726
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-11-03 11:47:18 -0500

    created js and html files and played with code along with video

---
+++ strings.js
@@ -0,0 +1,29 @@
+console.log( 'js' );
+
+let catName = 'Witten' ;
+let catAge = 3;
+
+console.log( catName );
+// concatenation with comma for console (comma only works in console.log)
+console.log( 'The name of the cat is:', catName );
+console.log( 'The age of the cat is:', catAge );
+
+//concatenate text with "+"
+let sentence = 'This is a tence about the cat named ' + catName;
+console.log( sentence );
+
+sentence = 'The cat is ' + catAge + ' years old.';
+console.log(sentence);
+
+catName = 'Max';
+catAge = 14;
+
+sentence = 'Later on, a different cat is ' + catAge + ' years old and is named ' + catName;
+console.log(sentence);
+
+
+
+
+
+
+
